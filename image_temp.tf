d8b2022e86b4a1af38cda7eedd85871ae9cc99e9
jmbrinkman <jmbrinkman@google.com>
2025-03-18 15:57:04 +0000

    dsa

--- image_temp.tf
+++ image_temp.tf
@@ -1,10 +1,9 @@
 resource "google_compute_image" "default" {
   name                   = "debian-11-image-template-uiuuiuiiu"
-  project                = var.gcp_project_id
+  project                =  "qwiklabs-gcp-00-fd1b34daa3bd"
   family                 = "debian-11"
   raw_disk {
     source = "https://storage.googleapis.com/gce-uefi-images/debian-11/debian-11.tar.gz"
   }
-  source_disk_project    = "gce-uefi-images"
-  storage_locations      = [var.gcp_region]
 }
+
--- lb.tf
+++ lb.tf
@@ -2,19 +2,19 @@ resource "google_compute_global_forwarding_rule" "default" {
   name                  = "cepf-infra-lb"
   target                = google_compute_target_http_proxy.default.id
   port_range            = "80"
-  project               = var.gcp_project_id
+  project               = "qwiklabs-gcp-00-fd1b34daa3bd"
 }
 
 resource "google_compute_target_http_proxy" "default" {
   name                  = "cepf-infra-lb-proxy"
   url_map               = google_compute_url_map.default.id
-  project               = var.gcp_project_id
+  project               = "qwiklabs-gcp-00-fd1b34daa3bd"
 }
 
 resource "google_compute_url_map" "default" {
   name                  = "cepf-infra-lb-url-map"
   description           = "a description"
-  project               = var.gcp_project_id
+  project               = "qwiklabs-gcp-00-fd1b34daa3bd"
   default_service       = google_compute_backend_service.default.id
 
   host_rule {
@@ -32,7 +32,7 @@ resource "google_compute_backend_service" "default" {
   name        = "cepf-infra-lb-backend-default"
   port_name   = "http"
   protocol    = "HTTP"
-  project     = var.gcp_project_id
+  project     = "qwiklabs-gcp-00-fd1b34daa3bd"
   timeout_sec = 10
   session_affinity = "GENERATED_COOKIE"
 
@@ -43,6 +43,6 @@ resource "google_compute_backend_service" "default" {
 
 resource "google_compute_instance_group" "default" {
   name = "cepf-infra-lb-instance-group"
-  zone = var.gcp_region
-  project = var.gcp_project_id
+  zone = "europe-west4"
+  project = "qwiklabs-gcp-00-fd1b34daa3bd"
 }
